e3a51c5439aa24181dbf418e8a6417b34db654c3
袁浩宇 <614199451@qq.com>
2019-07-19 01:05:38 +0800

    New

--- Src/main.cc
+++ Src/main.cc
@@ -3,8 +3,8 @@
 #include "Log/Log.h"
 #include "Util/Util.h"
 
-int	main(){
-	std::shared_ptr<Magic::Logger> logger = std::make_shared<Magic::Logger>();
+int main(){
+    std::shared_ptr<Magic::Logger> logger = std::make_shared<Magic::Logger>();
     logger->addILogAppender(std::shared_ptr<Magic::ILogAppender>(new Magic::FileLogAppender("debug.txt")));
     logger->setLevel(Magic::LogLevel::DEBUG);
     
