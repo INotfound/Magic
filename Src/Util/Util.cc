3e19005fe7dc6a7fd2918d5cff3216faa2364f1d
INotfound <INotfound@github.com>
2019-08-09 09:40:14 +0800

    New

--- Src/Util/Util.cc
+++
@@ -1,6 +0,0 @@
-#include "Util.h"
-
-uint64_t Magic::getFiberId(){
-    return 0;
-}
-
--- Src/Util/Util.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-namespace Magic{
-    uint64_t getThreadId();
-    uint64_t getFiberId();
-}
