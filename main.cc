85375011157590affbe0d8abedd2cd5bdeb2b0af
INotfound <INotfound@github.com>
2019-08-16 11:19:24 +0800

    New

--- main.cc
+++ main.cc
@@ -1,24 +1,26 @@
 #include <iostream>
 #include <memory>
 #include <time.h>
+#include <unistd.h>
 #include "Magic/Log/Log.h"
 #include "Magic/Util/Util.h"
 #include "Magic/Thread/Thread.h"
 #include "Magic/Thread/Mutex.h"
 
 static auto& g_rlog = MAGIC_LOG_ROOT();
-Magic::Mutex mutex;
 void bibo(){
-    Magic::Mutex::Lock lock(mutex);
-    MAGIC_LOG_INFO(g_rlog) << "hello XiaoBaiJun1";
-    std::cout.flush();
+    for(int i=0;i<10;i++)
+        MAGIC_LOG_INFO(MAGIC_LOG_NAME("root")) << "[root root root] " << i;
 }
+void momo(){
+    for(int i=0;i<10;i++)
+        MAGIC_LOG_INFO(MAGIC_LOG_NAME("xibo")) << "[xibo xibo xibo] " << i;
+}
+
 int main(){
     {
         Magic::Thread thread("111",&bibo);
-        Magic::Thread thread1("222",&bibo);
-        Magic::Thread thread2("333",&bibo);
-        getchar();
+        Magic::Thread thread1("222",&momo);
     }
     getchar();
 	return 0;
