52fde7d96d6f5ab8360e6bb07e5a74fa0f6af252
INotFound <614199451@qq.com>
2019-10-20 11:17:02 +0800

    deleted:    old

--- old/Magic/Magic.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Log/Log.h"
-#include "Fiber/Fiber.h"
-#include "Thread/Thread.h"
-#include "Util/Util.h"
-#include "Util/Macro.h"
-#include "Util/Singleton.h"
-#include "Util/Noncopyable.h"
