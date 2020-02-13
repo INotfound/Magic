4c1650245dbcfbdbd6b325cfbec744802bfb96d9
INotFound <INotFound@github.com>
2020-02-13 21:18:35 +0800

    change

--- Include/Magic.h
+++ Include/Magic.h
@@ -1,11 +1,24 @@
 #pragma once
 #include "Log.h"
+#include "Any.h"
+#include "Md5.h"
 #include "Core.h"
+#include "Gzip.h"
 #include "Macro.h"
 #include "Mutex.h"
+#include "IoPool.h"
 #include "Thread.h"
 #include "Config.h"
 #include "Plugin.h"
+#include "Adapter.h"
+#include "TcpServer.h"
+#include "Http/Http.h"
+#include "Http/Session.h"
+#include "Http/HttpFile.h"
+#include "Http/MultiPart.h"
+#include "Http/HttpParser.h"
+#include "Http/HttpServer.h"
+#include "Http/HttpServlet.h"
 
 namespace Magic {
     void Init(const std::string& Name);
