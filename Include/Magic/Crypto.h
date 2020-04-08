5c849addeb35426d1f4ff2777c67553318491021
INotfound <INotfound@github.com>
2020-04-08 16:18:59 +0800

    change 20/4/5

--- Include/Magic/Crypto.h
+++ Include/Magic/Crypto.h
@@ -6,8 +6,8 @@
  */
 #pragma once
 #include <string>
-#include <openssl/md5.h>
-#include <openssl/sha.h>
+#include "openssl/md5.h"
+#include "openssl/sha.h"
 namespace Magic {
     /**
      * @brief: MD5
